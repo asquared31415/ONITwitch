10854a7f8702162ea2e7f4e5c55944da58cfd53f
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2022-11-15 15:25:28 -0500

    data

---
+++ EventLib/DataManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace EventLib;
+
+public class DataManager
+{
+	private static DataManager instance;
+	private readonly Dictionary<EventInfo, object> storedData = new();
+	
+	public static DataManager Instance
+	{
+		get
+		{
+			instance ??= new DataManager();
+			return instance;
+		}
+	}
+
+	public void AddDataForEvent([NotNull] EventInfo info, object data)
+	{
+		storedData.Add(info, data);
+	}
+
+	[CanBeNull]
+	public object GetDataForEvent([NotNull] EventInfo info)
+	{
+		return storedData.TryGetValue(info, out var data) ? data : null;
+	}
+}
--- ONITwitch/OniTwitchMod.cs
+++ ONITwitch/OniTwitchMod.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using EventLib;
 using HarmonyLib;
 using JetBrains.Annotations;
 using KMod;
@@ -16,12 +17,12 @@ public class OniTwitchMod : UserMod2
 		base.OnLoad(harmony);
 		var eventInfo = EventManager.Instance.RegisterEvent("aaa");
 		Debug.Log("Triggering event after init");
-		var data = new Dictionary<string, object>();
-		EventManager.Instance.TriggerEvent(eventInfo, data);
+		var dictData = new Dictionary<string, object>();
+		EventManager.Instance.TriggerEvent(eventInfo, dictData);
 		Debug.Log("adding one listener");
 		EventManager.Instance.AddListenerForEvent(eventInfo, _ => { Debug.Log("Listener1"); });
 		Debug.Log("triggering again");
-		EventManager.Instance.TriggerEvent(eventInfo, data);
+		EventManager.Instance.TriggerEvent(eventInfo, dictData);
 		Debug.Log("adding second listener");
 
 		void Listener2(object _)
@@ -31,21 +32,36 @@ public class OniTwitchMod : UserMod2
 
 		EventManager.Instance.AddListenerForEvent(eventInfo, Listener2);
 		Debug.Log("triggering again");
-		EventManager.Instance.TriggerEvent(eventInfo, data);
+		EventManager.Instance.TriggerEvent(eventInfo, dictData);
 		Debug.Log("unregistering event 2 via reflection");
 		var eventManager = EventInterface.GetEventManagerInstance();
 		var info = eventManager.GetEventById("aaa")!;
 		eventManager.RemoveListenerForEvent(info, Listener2);
 
 		Debug.Log("triggering again normally");
-		EventManager.Instance.TriggerEvent(eventInfo, data);
+		EventManager.Instance.TriggerEvent(eventInfo, dictData);
 		Debug.Log("triggering again via reflection");
-		eventManager.TriggerEvent(info, data);
-		Debug.Log("adding third listener via reflection");
-		eventManager.AddListenerForEvent(info, _ => Debug.Log("Listener3"));
-		Debug.Log("triggering via reflection");
-		eventManager.TriggerEvent(info, data);
+		eventManager.TriggerEvent(info, dictData);
+		Debug.Log("adding printer listener via reflection");
+
+		void Printer(object data)
+		{
+			if (data is List<string> d)
+			{
+				foreach (var s in d)
+				{
+					Debug.Log(s);
+				}
+			}
+		}
+		eventManager.AddListenerForEvent(info, Printer);
+		
+		Debug.Log("adding list data for event");
+		DataManager.Instance.AddDataForEvent(eventInfo, new List<string>(){"abc", "def"});
+		Debug.Log("triggering via reflection with data");
+		var d = (List<string>) DataManager.Instance.GetDataForEvent(eventInfo);
+		eventManager.TriggerEvent(info, d);
 		Debug.Log("triggering again normally");
-		EventManager.Instance.TriggerEvent(eventInfo, data);
+		EventManager.Instance.TriggerEvent(eventInfo, d);
 	}
 }
