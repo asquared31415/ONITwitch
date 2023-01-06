0f5534e50101ef7d93620be171ee46323d290361
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-01-06 16:35:24 -0500

    fix pee event to make dupes pee immediately and not get stuck

--- ONITwitchCore/Commands/PeeCommand.cs
+++ ONITwitchCore/Commands/PeeCommand.cs
@@ -11,13 +11,12 @@ public class PeeCommand : CommandBase
 
 	public override void Run(object data)
 	{
-		var peeUrge = Db.Get().Urges.Pee;
 		foreach (var identity in Components.LiveMinionIdentities.Items)
 		{
-			if (identity.TryGetComponent<ChoreConsumer>(out var consumer) &&
-				identity.TryGetComponent<ChoreProvider>(out var provider))
+			var smi = identity.GetSMI<BladderMonitor.Instance>();
+			if ((smi != null) && identity.TryGetComponent<ChoreProvider>(out var provider))
 			{
-				consumer.AddUrge(peeUrge);
+				smi.GoTo(smi.sm.urgentwant);
 				provider.AddChore(new PeeChore(identity.gameObject.GetComponent<StateMachineController>()));
 			}
 		}
