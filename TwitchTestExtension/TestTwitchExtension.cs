889cfa174b2219abb3cd6f1a8e839c49e0680f43
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-04-11 12:00:05 -0400

    improve example mod

--- TwitchTestExtension/TestTwitchExtension.cs
+++ TwitchTestExtension/TestTwitchExtension.cs
@@ -14,9 +14,8 @@ public class TestTwitchExtension : UserMod2
 {
 }
 
-[HarmonyPatch(typeof(Db))]
-[HarmonyPatch("Initialize")]
-[HarmonyAfter("asquared31415.TwitchIntegration")]
+[HarmonyPatch(typeof(Db), "Initialize")]
+[HarmonyAfter(TwitchModInfo.StaticID)]
 // ReSharper disable once InconsistentNaming
 public static class Db_Initialize_Patch
 {
@@ -25,59 +24,36 @@ public static class Db_Initialize_Patch
 	{
 		if (!TwitchModInfo.TwitchIsPresent)
 		{
-			Log.Warn("[Test Ext Events] Twitch not enabled");
+			Log.Warn("[Test Extension Events] Twitch not enabled");
 			return;
 		}
 
 		var dataInst = DataManager.Instance;
 		var deckInst = TwitchDeckManager.Instance;
 
-		var (crash, crashGroup) = EventGroup.DefaultSingleEventGroup("ExtCrash", 0, "[DO NOT USE] CRASH EVENT");
-		crash.AddListener(
-			_ =>
-			{
-				[NotNull]
-				string FakeNotNull()
-				{
-					return null!;
-				}
-
-				// throws NRE
-				var unused = FakeNotNull().Length;
-			}
-		);
-		deckInst.AddGroup(crashGroup);
-
-		var (extEvent, extEventGroup) = EventGroup.DefaultSingleEventGroup("ExtEvent", 4, "Extended Event");
+		// Add a completely custom event
+		var (extEvent, extEventGroup) = EventGroup.DefaultSingleEventGroup("LogEvent", 4, "Extra Event");
 		extEvent.AddListener(
 			data => { Log.Info($"Triggered ext event with data {data}"); }
 		);
-		// add condition
-		extEvent.AddCondition(data => data is ExtData { Thing: true });
+		// add condition so the event only runs when its data is true
+		extEvent.AddCondition(data => (bool) data);
+		dataInst.SetDataForEvent(extEvent, true);
+		// This event causes no danger
 		extEvent.Danger = Danger.None;
-
-		// set up the data
-		dataInst.SetDataForEvent(extEvent, new ExtData(true));
-
 		// register the group into the deck
 		deckInst.AddGroup(extEventGroup);
 
-		var rainPrefabType = Type.GetType("ONITwitch.Commands.RainPrefabCommand, ONITwitch")!;
-		var commandBase = new CommandBase(rainPrefabType);
 
-		var (extRain, extRainGroup) = EventGroup.DefaultSingleEventGroup("CustomRainEvent", 1, "Custom Rain Event");
-		extRain.AddListener(commandBase.GetRunAction());
+		// Add an event that crashes to demonstrate the event crash handler
+		var (crash, crashGroup) = EventGroup.DefaultSingleEventGroup("Crash", 0, "[DO NOT USE] CRASH EVENT");
+		crash.AddListener(_ => throw new Exception("Explicit crash from a test event"));
+		deckInst.AddGroup(crashGroup);
 
-		dataInst.SetDataForEvent(
-			extRain,
-			new Dictionary<string, object> { { "PrefabId", "PropFacilityCouch" }, { "Count", 10.0d } }
-		);
-
-		deckInst.AddGroup(extRainGroup);
 
 		// manual group creation
-		var extGroup = EventGroup.GetOrCreateGroup("ExtGroup");
-		var manualGroupEvent = extGroup.AddEvent("ManualEvent", 1, "Manual Group Event");
+		var extGroup = EventGroup.GetOrCreateGroup("TwitchExtGroup");
+		var manualGroupEvent = extGroup.AddEvent("ManuallyAddedEvent", 1, "Manual Group Event");
 		manualGroupEvent.Danger = Danger.None;
 		manualGroupEvent.AddCondition(_ => false);
 		manualGroupEvent.AddListener(
@@ -85,6 +61,8 @@ public static class Db_Initialize_Patch
 		);
 		deckInst.AddGroup(extGroup);
 
+
+		// Group manipulation and on change event
 		void GroupChanged(EventGroup group)
 		{
 			Log.Info($"Group changed, total weight: {group.TotalWeight}");
@@ -100,9 +78,26 @@ public static class Db_Initialize_Patch
 		extGroup.SetWeight(manualGroupEvent, 1);
 		Log.Info("Done changing weights");
 
-		var group = TwitchDeckManager.Instance.GetGroup("TestExtGroup");
+
+		// Reflecting for a type that exists and using it in a custom event
+		// note: this is less reliable than rewriting code because events may change
+		var rainPrefabType = Type.GetType("ONITwitch.Commands.RainPrefabCommand, ONITwitch")!;
+		var commandBase = new CommandBase(rainPrefabType);
+		var (extRain, extRainGroup) = EventGroup.DefaultSingleEventGroup("CustomRainEvent", 1, "Custom Rain Event");
+		// Use the original event's run action as the action for this event too. You could also compose one or more events
+		// run action together or add code with this
+		extRain.AddListener(commandBase.GetRunAction());
+		// This data must be careful to follow the expected format to the command to not crash
+		dataInst.SetDataForEvent(
+			extRain,
+			new Dictionary<string, object> { { "PrefabId", "PropFacilityCouch" }, { "Count", 10.0d } }
+		);
+		deckInst.AddGroup(extRainGroup);
+
+
+		// Attempting to get a group that may or may not exist, and modify it
+		// In this case the group does not exist, so nothing will happen
+		var group = TwitchDeckManager.Instance.GetGroup("TestExtGroupDOES_NOT_EXIST");
 		group?.AddEvent("TestExtEvent", 0);
 	}
 }
-
-internal record struct ExtData(bool Thing);
