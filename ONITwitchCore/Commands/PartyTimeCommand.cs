d798f1540dbcdb77a4e2e16184a186eebadd326b
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-07-18 23:49:14 -0400

    make party time respect light sensitivity setting

--- ONITwitchCore/Commands/PartyTimeCommand.cs
+++ ONITwitchCore/Commands/PartyTimeCommand.cs
@@ -1,11 +1,18 @@
 using ONITwitch.Cmps;
 using ONITwitch.Patches;
+using ONITwitch.Settings;
 using ONITwitch.Toasts;
 
 namespace ONITwitch.Commands;
 
 internal class PartyTimeCommand : CommandBase
 {
+	public override bool Condition(object data)
+	{
+		// If photosensitive mode is enabled, this event must not appear 
+		return !GenericModSettings.GetConfig().PhotosensitiveMode;
+	}
+
 	public override void Run(object data)
 	{
 		var time = (float) (double) data;
@@ -13,7 +20,10 @@ internal class PartyTimeCommand : CommandBase
 		partyTime.TimeRemaining = time;
 		partyTime.enabled = true;
 		PartyTimePatch.Enabled = true;
-		
-		ToastManager.InstantiateToast(STRINGS.ONITWITCH.TOASTS.PARTY_TIME.TITLE, STRINGS.ONITWITCH.TOASTS.PARTY_TIME.BODY);
+
+		ToastManager.InstantiateToast(
+			STRINGS.ONITWITCH.TOASTS.PARTY_TIME.TITLE,
+			STRINGS.ONITWITCH.TOASTS.PARTY_TIME.BODY
+		);
 	}
 }
