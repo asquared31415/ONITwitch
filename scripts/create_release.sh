90919e676edfcb64cc76b7d29517ef47cdd924bb
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2023-12-20 12:12:46 -0500

    add test docs script

--- scripts/create_release.sh
+++ scripts/create_release.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ $# -lt 1 ]
 then
     echo "Enter the version of the release"
--- scripts/test_docs.sh
+++ scripts/test_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Generating changelogs..."
+echo "[TESTING] Generating test changelogs..."
 cargo run --manifest-path scripts/gen_changelog/Cargo.toml -- changelogs/
 
 rm -rf live_docs
@@ -7,12 +7,4 @@ git clone git@github.com:asquared31415/asquared31415.github.io.git live_docs
 echo "Clearing old docs..."
 rm -rf live_docs/twitchintegration/dev_docs/*
 echo "Running docfx"
-docfx
-echo "Committing"
-cd live_docs
-git add .
-git commit -m "Twitch Integration docs update" --quiet
-COMMIT=$(git log --pretty=format:'%h' -n 1)
-echo "Committed $COMMIT"
-git push
-cd ..
+docfx --serve
