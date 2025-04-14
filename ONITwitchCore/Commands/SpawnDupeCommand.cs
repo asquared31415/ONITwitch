a26be7e1fed3659e44df38b95d13316bdc3d0260
asquared31415 <34665709+asquared31415@users.noreply.github.com>
2025-04-14 11:00:14 -0400

    prevent bionic duplicants from being spawned by chat

--- ONITwitchCore/Commands/SpawnDupeCommand.cs
+++ ONITwitchCore/Commands/SpawnDupeCommand.cs
@@ -11,134 +11,118 @@ using UnityEngine;
 
 namespace ONITwitch.Commands;
 
-internal class SpawnDupeCommand : CommandBase
-{
-	private static readonly Dictionary<string, SpecialDupeData> SpecialDupes = new()
-	{
-		{ "asquared31415", new SpecialDupeData("NAILS") },
-	};
+internal class SpawnDupeCommand : CommandBase {
+    private static readonly Dictionary<string, SpecialDupeData> SpecialDupes =
+        new() { { "asquared31415", new SpecialDupeData("NAILS") } };
 
-	public override bool Condition(object data)
-	{
-		var maxDupes = (double) ((IDictionary<string, object>) data)["MaxDupes"];
-		return Components.MinionIdentities.Count < maxDupes;
-	}
+    public override bool Condition(object data) {
+        var maxDupes = (double)((IDictionary<string, object>)data)["MaxDupes"];
+        return Components.MinionIdentities.Count < maxDupes;
+    }
 
-	public override void Run(object data)
-	{
-		string name = null;
-		Color? color = null;
+    public override void Run(object data) {
+        var config = TwitchSettings.GetConfig();
 
-		var config = TwitchSettings.GetConfig();
+        ChosenVoter? chosenVoter = null;
 
-		if ((VoteController.Instance != null) && (VoteController.Instance.CurrentVote != null))
-		{
-			var votes = VoteController.Instance.CurrentVote.GetUserVotes().ToList();
-			// Only get users that are not disallowed and are not spawned yet.
-			var allowedVotes = votes.Where(
-					pair =>
-					{
-						if (config.DisallowedDupeNames.Any(
-								disallowed => string.Equals(
-									pair.Key.DisplayName,
-									disallowed,
-									StringComparison.InvariantCultureIgnoreCase
-								)
-							))
-						{
-							return false;
-						}
+        if (VoteController.Instance != null && VoteController.Instance.CurrentVote != null) {
+            var votes = VoteController.Instance.CurrentVote.GetUserVotes().ToList();
+            // Only get users that are not disallowed and are not spawned yet.
+            var allowedVotes = votes.Where(
+                    pair => {
+                        if (config.DisallowedDupeNames.Any(
+                                disallowed => string.Equals(
+                                    pair.Key.DisplayName,
+                                    disallowed,
+                                    StringComparison.InvariantCultureIgnoreCase
+                                )
+                            )) {
+                            return false;
+                        }
 
-						return !Components.LiveMinionIdentities.Items.Any(
-							([NotNull] i) =>
-							{
-								var normalizedName = i.name.ToLowerInvariant();
-								return normalizedName.Contains(pair.Key.DisplayName.ToLowerInvariant());
-							}
-						);
-					}
-				)
-				.ToList();
+                        return !Components.LiveMinionIdentities.Items.Any(
+                            ([NotNull] i) => {
+                                var normalizedName = i.name.ToLowerInvariant();
+                                return normalizedName.Contains(pair.Key.DisplayName.ToLowerInvariant());
+                            }
+                        );
+                    }
+                )
+                .ToList();
 
-			if (allowedVotes.Count > 0)
-			{
-				var (user, _) = allowedVotes.GetRandom();
-				name = user.DisplayName;
-				color = user.NameColor;
-			}
-		}
+            if (allowedVotes.Count > 0) {
+                (var user, var _) = allowedVotes.GetRandom();
+                chosenVoter = new ChosenVoter(user.DisplayName, user.NameColor);
+            }
+        }
 
-		var liveMinions = Components.LiveMinionIdentities.Items;
-		if (liveMinions.Count == 0)
-		{
-			Log.Warn("No live minions, aborting spawn");
-			ToastManager.InstantiateToast(
-				STRINGS.ONITWITCH.TOASTS.WARNINGS.EVENT_FAILURE,
-				STRINGS.ONITWITCH.TOASTS.WARNINGS.SPAWN_DUPE_FAILURE.BODY
-			);
-			return;
-		}
+        var liveMinions = Components.LiveMinionIdentities.Items;
+        if (liveMinions.Count == 0) {
+            Log.Warn("No live minions, aborting spawn");
+            ToastManager.InstantiateToast(
+                STRINGS.ONITWITCH.TOASTS.WARNINGS.EVENT_FAILURE,
+                STRINGS.ONITWITCH.TOASTS.WARNINGS.SPAWN_DUPE_FAILURE.BODY
+            );
+            return;
+        }
 
-		var minion = Util.KInstantiate(Assets.GetPrefab(MinionConfig.ID));
-		minion.name = Assets.GetPrefab(MinionConfig.ID).name;
-		Immigration.Instance.ApplyDefaultPersonalPriorities(minion);
-		minion.SetActive(true);
+        var minion = Util.KInstantiate(Assets.GetPrefab(MinionConfig.ID));
+        minion.name = Assets.GetPrefab(MinionConfig.ID).name;
+        Immigration.Instance.ApplyDefaultPersonalPriorities(minion);
+        minion.SetActive(true);
 
-		var identity = minion.GetComponent<MinionIdentity>();
-		if (name != null)
-		{
-			if (SpecialDupes.TryGetValue(name, out var specialDupeData))
-			{
-				var personalities = Db.Get().Personalities;
-				var personality = personalities.TryGet(specialDupeData.PersonalityId) ??
-								  personalities.GetRandom(true, false);
-				new MinionStartingStats(personality).Apply(minion);
-			}
-			else
-			{
-				new MinionStartingStats(false).Apply(minion);
-			}
+        ApplyPersonality(minion, chosenVoter, config.UseTwitchNameColors);
 
-			var finalColor = color ?? ColorUtil.GetRandomTwitchColor();
-			identity.SetName(
-				config.UseTwitchNameColors
-					? $"<color=#{finalColor.ToHexString()}>{name}</color>"
-					: name
-			);
-		}
-		else
-		{
-			new MinionStartingStats(false).Apply(minion);
-		}
+        Vector3 pos;
+        // First try to find a printing pod, since that should always be in a safe location.
+        var pods = Components.Telepads.Items;
+        if (pods.Count > 0) {
+            pos = pods.GetRandom().transform.position;
+        } else {
+            Log.Debug("Unable to find any Telepads, using a random dupe's location instead");
+            pos = liveMinions.GetRandom().transform.position;
+        }
 
-		identity.GetComponent<MinionResume>().ForceAddSkillPoint();
+        minion.transform.SetLocalPosition(pos);
 
-		Vector3 pos;
-		// First try to find a printing pod, since that should always be in a safe location.
-		var pods = Components.Telepads.Items;
-		if (pods.Count > 0)
-		{
-			pos = pods.GetRandom().transform.position;
-		}
-		else
-		{
-			Log.Debug("Unable to find any Telepads, using a random dupe's location instead");
-			pos = liveMinions.GetRandom().transform.position;
-		}
+        var identity = minion.GetComponent<MinionIdentity>();
+        identity.GetComponent<MinionResume>().ForceAddSkillPoint();
+        var upgradeFx = new UpgradeFX.Instance(identity, new Vector3(0.0f, 0.0f, -0.1f));
+        upgradeFx.StartSM();
 
-		minion.transform.SetLocalPosition(pos);
+        ToastManager.InstantiateToastWithGoTarget(
+            STRINGS.ONITWITCH.TOASTS.SPAWN_DUPE.TITLE,
+            string.Format(STRINGS.ONITWITCH.TOASTS.SPAWN_DUPE.BODY_FORMAT, identity.name),
+            minion
+        );
 
-		var upgradeFx = new UpgradeFX.Instance(identity, new Vector3(0.0f, 0.0f, -0.1f));
-		upgradeFx.StartSM();
+        Log.Info($"Spawned duplicant {identity.name}");
+    }
 
-		ToastManager.InstantiateToastWithGoTarget(
-			STRINGS.ONITWITCH.TOASTS.SPAWN_DUPE.TITLE,
-			string.Format(STRINGS.ONITWITCH.TOASTS.SPAWN_DUPE.BODY_FORMAT, identity.name),
-			minion
-		);
+    private static void ApplyPersonality(GameObject minion, ChosenVoter? voter, bool useTwitchColor) {
+        if (voter is (var name, var color)) {
+            if (SpecialDupes.TryGetValue(name, out var specialDupeData)) {
+                var personalities = Db.Get().Personalities;
 
-		Log.Info($"Spawned duplicant {identity.name}");
-	}
+                var personality = personalities.TryGet(specialDupeData.PersonalityId);
+                if (personality == null || !Game.IsDlcActiveForCurrentSave(personality.requiredDlcId)) {
+                    personality = personalities.GetRandom(GameTags.Minions.Models.Standard, true, false);
+                }
 
-	private record struct SpecialDupeData([NotNull] string PersonalityId);
+                new MinionStartingStats(personality).Apply(minion);
+            } else {
+                new MinionStartingStats(GameTags.Minions.Models.Standard, false).Apply(minion);
+            }
+
+            var finalColor = color ?? ColorUtil.GetRandomTwitchColor();
+            minion.GetComponent<MinionIdentity>()
+                .SetName(useTwitchColor ? $"<color=#{finalColor.ToHexString()}>{name}</color>" : name);
+        } else {
+            new MinionStartingStats(GameTags.Minions.Models.Standard, false).Apply(minion);
+        }
+    }
+
+    private record struct ChosenVoter([NotNull] string Name, Color? Color);
+
+    private record struct SpecialDupeData([NotNull] string PersonalityId);
 }
